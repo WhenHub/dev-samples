fd69182fcbded8f06a324eca269f538caa4456b2
Traci Fong <tre.fong1@gmail.com>
2017-08-10 00:54:14 -0700

    Add nav bar with logo

---
+++ index.js
@@ -0,0 +1,22 @@
+document.getElementById("navMenu").innerHTML =
+    "<nav class='navbar navbar-inverse'>" +
+        "<div class='container-fluid'>" +
+            "<div class='navbar-header'>" +
+            "<a class='pull-left' href='https://www.whenhub.com/'>" +
+                "<img src='https://cdn.whenhub.com/img/logo/logo-185x36.png' style='padding: 7px;'>" +
+            "</a>" +
+            "</div>" +
+            "<ul class='nav navbar-nav'>" +
+                "<li class='first-item active'><a href='../index.html'>Developer Samples Home</a></li>" +
+                "<li class='second-item'><a href='JavascriptEmbed/view.html'>JavaScript Embed</a></li>" +
+                "<li class='third-item dropdown'>" +
+                    "<a class='dropdown-toggle' data-toggle='dropdown' href='#'>Display Samples" +
+                    "<span class='caret'></span></a>" +
+                    "<ul class='dropdown-menu'>" + 
+                        "<li><a href='#'>Angular Sample</a></li>" +
+                        "<li><a href='#'>Jquery Sample</a></li>" +
+                    "</ul>" +
+                "</li>" +
+            "</ul>" +
+        "</div>" +
+    "</nav>"
