97d2d02938fa4e63bb75b6f6e1d28e9d55631595
Traci Fong <tre.fong1@gmail.com>
2017-08-11 03:32:47 -0700

    Make functional API call; have taken out personal access token

--- Display/JquerySample/jquery-demo.js
+++ Display/JquerySample/jquery-demo.js
@@ -1,6 +1,36 @@
-console.log(document.title);
-console.log("hello world");
-
-// select correct nav item
+// Highlight correct nav item
 $('.active').removeClass();
 $('.third-item').addClass('active');
+
+//Get Schedules
+var data = JSON.stringify(false);
+
+var xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener("readystatechange", function () {
+  if (this.readyState === 4) {
+    var jsonResponse = JSON.parse(this.responseText);
+    console.log(jsonResponse);
+
+    for (i = 0; i < jsonResponse.length; i++) {
+        $('.schedules').append(
+            '<div>' + jsonResponse[i].name +
+            '</div>'
+
+        );
+    }
+
+
+
+}
+});
+
+xhr.open("GET", "https://api.whenhub.com/api/users/me/schedules");
+xhr.setRequestHeader("authorization", "ACCESS_TOKEN");
+
+xhr.send(data);
+
+
+
+
