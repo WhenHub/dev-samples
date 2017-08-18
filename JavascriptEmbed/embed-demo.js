d7a63f5a70b9991656b71ac67e21222c02c3bd73
Traci Fong <tre.fong1@gmail.com>
2017-08-18 14:20:55 -0700

    Add readme files to each project

--- JavascriptEmbed/embed-demo.js
+++ JavascriptEmbed/embed-demo.js
@@ -1,16 +1,13 @@
 var wcast = {};
-// Got schedule id from embed page of whencast
+// Get schedule id from embed page of whencast
 wcast['schedule-id'] = "59464f0ec0b33f22e4bcfbb9";        
 wcast['theme'] = 'teal';
 var height = $(window).height() - 100;
 if (height > 800) {
   height = 800;
 }
-// if you are running local host use this code
-$('#sample').html('<div data-whencast="test" data-viz-url-base="https://viz.whenhub.com/" data-app-url-base="https://studio.whenhub.com/" data-api-url-base="https://api.whenhub.com/api/" style="height:' + height + 'px;"></div>');
 
-// otherwise use this code
-// $('#sample').html('<div data-whencast="test" style="height:' + height + 'px;"></div>');
+$('#sample').html('<div data-whencast="test" data-viz-url-base="https://viz.whenhub.com/" data-app-url-base="https://studio.whenhub.com/" data-api-url-base="https://api.whenhub.com/api/" style="height:' + height + 'px;"></div>');
 
 // select where to place whencast
 var node = document.querySelector('div[data-whencast="test"]');
---
+++ display/jquerySample/README.md
@@ -0,0 +1,7 @@
+# WhenHub Jquery Sample
+This sample uses Jquery to display data on a page.
+
+
+## Access Token
+
+Your access token is required to load data to this project. Your access token can be found on your [WhenHub Studio Profile page](https://studio.whenhub.com/account). The access token variable can be updated in the jquery-demo.js file.
