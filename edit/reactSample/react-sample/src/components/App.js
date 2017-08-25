7060dc028b315ff29234e70113a67351b8a37a8d
Traci Fong <tre.fong1@gmail.com>
2017-08-24 22:55:55 -0700

    Display schedule imgs and info successfully

--- edit/reactSample/react-sample/src/components/App.js
+++ edit/reactSample/react-sample/src/components/App.js
@@ -9,7 +9,8 @@ class App extends Component {
 
     this.state = {
       accessToken: "LEnX1JDgECCh4B7PuEqy3SMSiUkbjHXKeKleyKzDBbbYyOO9ZloBfEWALkGjEz20", 
-      whencastName: [],
+      whencastIds: [],
+      whencasts: []
     }
   }
 
@@ -21,15 +22,16 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <div>
-          <Whencast data={this.state}/>
+          <Whencast data={this.state.whencasts}/>
         </div>
       </div>
     );
   }
-  fetchApi(url) {
+
+  fetchSchedules(url) {
     
         fetch(url, {
-          method: 'Get',
+          method: 'GET',
           dataType: 'json',
           headers: {
             'Accept': 'application/json',
@@ -38,13 +40,19 @@ class App extends Component {
           }
         })
         .then((res) => res.json()).then((data) => {
-          for (let i = 0; i < data.length; i++){
+          for (let i =0; i < data.length; i++){
             this.setState({
-              whencastName: this.state.whencastName.concat(data[i].name)
+              whencastIds: this.state.whencastIds.concat(data[i].id)
             })
-            
           }
-          console.log(this.state.whencastName)
+          console.log(this.state.whencastIds)
+          const ids = this.state.whencastIds;
+          for (let i=0; i < ids.length; i++){
+            this.fetchScheduleMedia(ids[i]);
+          }
+
+
+
         })
     
         // .catch((err) => console.log('Movie not found!'))
@@ -52,10 +60,36 @@ class App extends Component {
   } 
 
 
+  fetchScheduleMedia(Id){
+    let url = `https://api.whenhub.com/api/schedules/${Id}?filter[include]=media&filter[include][events]=media`
+    fetch(url, {
+      method: 'GET',
+      dataType: 'json',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': this.state.accessToken
+      }
+    })
+    .then((res) => res.json()).then((data) =>{
+        this.setState({
+          whencasts: this.state.whencasts.concat(data)
+        })
+      console.log(this.state.whencasts)
+
+    })
+
+
+  }
+
+
   componentDidMount() {
     let url = `https://api.whenhub.com/api/users/me/schedules`
-    this.fetchApi(url)
+    this.fetchSchedules(url)
   }
+
+
+
 
 }
 
--- edit/reactSample/react-sample/src/components/whencast.js
+++ edit/reactSample/react-sample/src/components/whencast.js
@@ -4,11 +4,23 @@ import React, { Component } from 'react';
 class Whencast extends Component {
     render() {
         let data = this.props.data;
+        console.log(data);
         return(
-
             <div>
-                {data.whencastName}
-            </div>
+                <h1>Whencasts!</h1>
+                {data.map(function(whencast) {
+                    return (
+                        <div key={whencast.id} className="whencast">
+                            {whencast.media.length > 0 &&
+                                <img src={whencast.media[0].url} alt="test"/>
+                            }
+                            <p>{whencast.name}</p>
+                            <p>{whencast.description}</p>
+
+                        </div>
+                    );
+                })}
+            </div>        
         );
     }
 };
